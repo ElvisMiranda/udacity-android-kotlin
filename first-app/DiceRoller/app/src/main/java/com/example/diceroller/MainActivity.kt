965753e5b69d3ddc42a5fdbebfb096bc19c96053
Elvis Miranda <elvis.miranda@live.com>
2023-05-14 06:04:29 -0400

    Exercuse. Finding Views Efficiently

--- first-app/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ first-app/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,14 +1,15 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,11 +18,13 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
-        val diceImage: ImageView = findViewById(R.id.dice_image)
+
         var drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
@@ -31,6 +34,7 @@ class MainActivity : AppCompatActivity() {
             6 -> R.drawable.dice_6
             else -> { R.drawable.empty_dice }
         }
+
         diceImage.setImageResource(drawableResource)
     }
 }
