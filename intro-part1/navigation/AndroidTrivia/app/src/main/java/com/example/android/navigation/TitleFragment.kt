4d42d9e595311d76450dc65865ec398e4156df48
Elvis Miranda <elvis.miranda@live.com>
2023-05-15 16:50:30 -0400

    Exercise. Let's Navigate Already

--- intro-part1/navigation/AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ intro-part1/navigation/AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+//import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -13,10 +15,22 @@ class TitleFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         // Inflate the layout for this fragment
-        val binding: FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        val binding: FragmentTitleBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+
+        // Use the navController to navigate using the titleFragment to gameFragment action by
+        // calling navigate(R.id.action_titleFragment_to_gameFragment)
+        // binding.playButton.setOnClickListener {view: View ->
+        //    view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        // }
+
+        // We can replace the anonymous function with the
+        // Navigation.createNavigateOnClickListener call
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
 
         return binding.root
     }
