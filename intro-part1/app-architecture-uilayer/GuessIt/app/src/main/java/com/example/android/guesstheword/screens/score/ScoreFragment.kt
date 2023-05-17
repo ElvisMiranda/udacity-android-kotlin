9c966d948e641dbcb14069bad32536d61ad87a4a
Elvis Miranda <elvis.miranda@live.com>
2023-05-16 23:56:10 -0400

    Exercise. Add a ViewModelFactory

--- intro-part1/app-architecture-uilayer/GuessIt/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ intro-part1/app-architecture-uilayer/GuessIt/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,6 +22,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
@@ -31,6 +33,9 @@ import com.example.android.guesstheword.databinding.ScoreFragmentBinding
  * Fragment where the final score is shown, after the game is over
  */
 class ScoreFragment : Fragment() {
+
+    private lateinit var viewModel: ScoreViewModel
+    private lateinit var viewModelFactory: ScoreViewModelFactory
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -50,6 +55,23 @@ class ScoreFragment : Fragment() {
         val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
         binding.scoreText.text = scoreFragmentArgs.score.toString()
         binding.playAgainButton.setOnClickListener { onPlayAgain() }
+
+        viewModelFactory = ScoreViewModelFactory(scoreFragmentArgs.score)
+        viewModel = ViewModelProvider(viewModelStore, viewModelFactory)[ScoreViewModel::class.java]
+
+        viewModel.apply {
+            // Add observer for score
+            score.observe(viewLifecycleOwner) { newScore ->
+                binding.scoreText.text = newScore.toString()
+            }
+
+            eventPlayAgain.observe(viewLifecycleOwner) { playAgain ->
+                if (playAgain) {
+                    findNavController().navigate(ScoreFragmentDirections.actionRestart())
+                    viewModel.onPlayAgainComplete()
+                }
+            }
+        }
 
         return binding.root
     }
