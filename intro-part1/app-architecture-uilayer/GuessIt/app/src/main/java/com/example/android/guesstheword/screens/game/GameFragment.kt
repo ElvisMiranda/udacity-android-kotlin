94dbf02a5aef29c84677061643fec13eec9d40f3
Elvis Miranda <elvis.miranda@live.com>
2023-05-16 21:37:17 -0400

    Update gradle settings + fixes

--- intro-part1/app-architecture-uilayer/GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ intro-part1/app-architecture-uilayer/GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -22,7 +22,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
 
